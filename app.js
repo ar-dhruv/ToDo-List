d135bee581a01a24dd20436950a98cb26527a0ce
Dhruv <dev.aroradhruv@gmail.com>
2021-11-14 09:01:16 +0530

    Search Functionality added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 //ADDING TODOS
 
@@ -37,6 +38,27 @@ list.addEventListener("click", (e) => {
   }
 });
 
-
 //SEARCH TODOS
 
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => {
+      return !todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      todo.classList.add("filtered");
+    });
+
+  Array.from(list.children)
+    .filter((todo) => {
+      return todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      todo.classList.remove("filtered");
+    });
+};
+
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
